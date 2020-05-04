0e5b25df5ca76713c6c057a6df0fb18ad802a3a0
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-05-04 05:28:42 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,15 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition 
+  def initialize(brand)
+    @brand = brand
+    @color = color
+    @size = size
+    @material = material
+    @condition = condition 
+  end
+
+
+
+
+
+end
