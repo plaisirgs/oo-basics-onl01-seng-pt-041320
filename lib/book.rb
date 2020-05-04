7ea6a47ab5b381e65d12eb4c7d1b1f34df260d6f
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-05-04 05:26:04 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,14 +1,14 @@
 class Book
-  attr_accessor :title,  :author, :count, :genre
+  attr_accessor :title,  :author, :page_count, :genre
   def initialize(title)
     @title = title
     @author = author
-    @count = count
+    @page_count = count
     @genre = genre
 end
 
   def turn_page
-  
+  puts "Flipping the page...wow, you read fast!"
 end
 end
 
